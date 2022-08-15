eb235d85704de83516ccbc79ce39a15984a39b03
Ali Azaz <ajaz.ali186@gmail.com>
2022-08-16 00:01:25 +0530

    First changes done by me
    First changes done by me

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import { MoralisProvider } from "react-moralis";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+    serverUrl='https://bejpnplqc6hn.usemoralis.com:2053/server'
+    appId='v8USR4jNem4mD8IvKg7O1VzkF4tCpiScigICzn8N'
+    >
+      <Component {...pageProps} />
+    </MoralisProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
